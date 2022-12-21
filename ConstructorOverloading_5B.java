907a9bfcdb0b44995a0b71be8cd2695ffcc589c1
Nishith P Shetty <69454628+nishith-p-shetty@users.noreply.github.com>
2022-12-21 23:22:12 +0530

    update

--- ConstructorOverloading_5B.java
+++ ConstructorOverloading_5B.java
@@ -14,8 +14,6 @@ public class ConstructorOverloading_5B
 {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
-
-        sc.close();
+        System.out.println();
     }
 }
