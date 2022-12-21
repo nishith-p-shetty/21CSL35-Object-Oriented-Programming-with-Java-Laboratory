3e9f4fbfcf0ee75caa18defc539d83f2bd415394
Nishith P Shetty <69454628+nishith-p-shetty@users.noreply.github.com>
2022-12-21 23:17:35 +0530

    Update MethodOverloading_5A.java

--- MethodOverloading_5A.java
+++ MethodOverloading_5A.java
@@ -5,17 +5,36 @@
         Write a java program demonstrating Method overloading.
     
     OUTPUT :
-        a
+            110
+            6.0
+            51.0
 */
 
 import java.util.Scanner;
+
+class MethodOverloading
+{
+    int add(int a, int b)
+    {
+        return (a + b);
+    }
+    float add(float a, float b)
+    {
+        return (a + b);
+    }
+    double add(int a, double b, double c)
+    {
+        return (a + b +c);
+    }
+}
 
 public class MethodOverloading_5A
 {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
-
-        sc.close();
+        MethodOverloading mobj = new MethodOverloading();
+        System.out.println( mobj.add(50, 60) );
+        System.out.println( mobj.add(3.5f, 2.5f) );
+        System.out.println( mobj.add(10, 30.5, 10.5) );
     }
 }
