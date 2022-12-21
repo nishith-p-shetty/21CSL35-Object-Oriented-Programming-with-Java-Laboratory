cb29584a77686cbea70cb82fa35a82b2c73aceda
Nishith P Shetty <69454628+nishith-p-shetty@users.noreply.github.com>
2022-12-21 22:28:20 +0530

    Update QuadraticEquation_1.java

--- QuadraticEquation_1.java
+++ QuadraticEquation_1.java
@@ -6,7 +6,20 @@
         ax2+bx+c=0. Read in a, b, c and use the quadratic formula.
     
     OUTPUT :
-        a
+        1.) Enter the value of a : 1
+            Enter the value of b : 1
+            Enter the value of c : -1
+            The Roots are 0.6180339887498949 and -1.618033988749895
+
+        2.) Enter the value of a : 1
+            Enter the value of b : 2
+            Enter the value of c : 3
+            Roots are not real.
+
+        3.) Enter the value of a : 1
+            Enter the value of b : 2
+            Enter the value of c : 1
+            The Roots is -1.0
 */
 
 import java.util.Scanner;
@@ -15,6 +28,29 @@ public class QuadraticEquation_1
 {
     public static void main(String[] args)
     {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of a : ");
+        double a = sc.nextDouble();
+        System.out.print("Enter the value of b : ");
+        double b = sc.nextDouble();
+        System.out.print("Enter the value of c : ");
+        double c = sc.nextDouble();
+        double d = (b * b) - (4.0 * a * c);
+        if (d > 0.0)
+        {
+            double r1 = (-b + Math.pow(d, 0.5) ) / (2.0 * a);
+            double r2 = (-b - Math.pow(d, 0.5) ) / (2.0 * a);
+            System.out.println("The Roots are " + r1 + " and " + r2);
+        }
+        else if (d == 0.0)
+        {
+            double r1 = -b / (2.0 * a);
+            System.out.println("The Roots is " + r1);
+        }
+        else
+        {
+            System.out.println("Roots are not real.");
+        }
+        sc.close();
     }
 }
